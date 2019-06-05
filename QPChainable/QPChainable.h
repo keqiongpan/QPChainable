1fe68218a6b7898eb56c44fafe1605ed4a4b0d20
Qiongpan Ke <keqiongpan@163.com>
2019-06-05 17:37:57 +0800

    [创建] 创建QPChainable.framework工程，并设置一些基本信息。

---
+++ QPChainable/QPChainable.h
@@ -0,0 +1,19 @@
+//
+//  QPChainable.h
+//  QPChainable
+//
+//  Created by keqiongpan@163.com on 2019/6/5.
+//  Copyright © 2019 Qiongpan Ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QPChainable.
+FOUNDATION_EXPORT double QPChainableVersionNumber;
+
+//! Project version string for QPChainable.
+FOUNDATION_EXPORT const unsigned char QPChainableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QPChainable/PublicHeader.h>
+
+
