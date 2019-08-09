5190cc80d295d5fbc1122ab3ad737b40da6ce180
Qiongpan Ke <keqiongpan@163.com>
2019-08-09 10:48:03 +0800

    [新增] 添加QPChainableEvaluation函数宏，用于支持有及早求值（eager evaluation）和惰性求值（lazy evaluation）要求的链式表达式函数。

--- QPChainable/Utilities.h
+++ QPChainable/Utilities.h
@@ -65,3 +65,51 @@
 #define QPChainableDictionary(keytype, valuetype, ... /* __VA_ARGS__ */) \
 /**/    ((NSDictionary<keytype, valuetype> *)@__VA_ARGS__)
 #endif
+
+
+#pragma mark - QPChainableEvaluation(resulttype, self, __VA_ARGS__)
+
+/**
+ *  将宏函数的`...'形参（实参为 expression 或 { expression; } 的形式）转换为合适
+ *  的表达式计算的block返回。`...'形参有两种表示方式，直接书写的expression表示立
+ *  即计算表达式的结果，并作为后续每次调用该block时的固定返回结果；而使用花括号
+ *  `{}'括起的expression表示后续每次调用该block时都重新计算表达式的结果返回。需
+ *  要注意的是，使用花括号`{}'括起来时，相当于行内代码块`({...})'，可以书写多行
+ *  语句，最后一行为返回结果，并需要添加分号`;'作为语句行结尾。
+ *
+ *  @note 表达式中出现的self将会被指向selfvalue参数的值，并且返回的block弱引用该
+ *      参数的值，所以即使不作特殊处理，在表达式中使用self也不会产生循环引用。
+ *
+ *  @example 使用示例，如下：
+ *
+ *      #define appellation(...) \
+ *              appellation(QPChainableEvaluation(NSString *, self, __VA_ARGS__))
+ *      - QP_CHAINABLE_DECLARE(appellation, (NSString *(^)(void)));
+ *
+ *      // 一次性设置一个值。
+ *      person.appellation([self.lastName stringByAppendingString:@"老师"])
+ *
+ *      // 每次使用时重新计算结果。
+ *      person.appellation({
+ *          NSString *title = self.male ? @"先生" : @"女士";
+ *          [self.lastName stringByAppendingString:title];
+ *      })
+ */
+#ifndef QPChainableEvaluation
+#define QPChainableEvaluation(resulttype, selfvalue, ...) \
+/**/    ({ \
+/**/        NSString *_expression = @#__VA_ARGS__; \
+/**/        resulttype (^_inputBlock)(__typeof(selfvalue) self); \
+/**/        resulttype (^_outputBlock)(void); \
+/**/        _inputBlock = ^resulttype (__typeof(selfvalue) self) { return (__VA_ARGS__); }; \
+/**/        if ([_expression hasPrefix:@"{"] && [_expression hasSuffix:@"}"]) { \
+/**/            __weak __typeof(selfvalue) _weakSelf = (selfvalue); \
+/**/            _outputBlock = ^resulttype (void) { return _inputBlock(_weakSelf); }; \
+/**/        } \
+/**/        else { \
+/**/            resulttype _resultValue = _inputBlock(selfvalue); \
+/**/            _outputBlock = ^resulttype (void) { return _resultValue; }; \
+/**/        } \
+/**/        _outputBlock; \
+/**/    })
+#endif
